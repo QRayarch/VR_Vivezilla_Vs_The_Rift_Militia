c18d66a321a2acd61c92e0b9357edf5b8dac6312
Colton Crawford (RIT Student) <cac5432@ad.rit.edu>
2016-12-01 18:54:02 -0500

    Hopefully got zilla wokring over the network.

---
+++ Assets/Scripts/CopyTransform.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CopyTransform : MonoBehaviour {
+
+    public Transform trans;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateTransform();
+	}
+
+    void UpdateTransform()
+    {
+        this.transform.position = trans.position;
+        this.transform.rotation = trans.rotation;
+    }
+}
---
+++ Assets/Scripts/Net/PlayerSelector.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.VR;
+
+public class PlayerSelector : NetworkBehaviour
+{
+    public enum Mode
+    {
+        Spectator,
+        Vive,
+        Rift,
+    }
+
+    public GameObject vivePlayer;
+    public GameObject spectorPlayer;
+
+    public void Start()
+    {
+
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        if (VRDevice.model.Contains("Vive"))
+        {
+            //LOAD IN THE VIVEZILLA HAHAH
+            CmdInstancePlayer(Mode.Vive);
+            return;
+        }
+
+        CmdInstancePlayer(Mode.Spectator);
+    }
+
+    [Command]
+    public void CmdInstancePlayer(Mode mode)
+    {
+        vivePlayer.SetActive(false);
+        spectorPlayer.SetActive(false);
+
+        if (mode == Mode.Vive)
+        {
+            vivePlayer.SetActive(true);
+        }
+        if(mode == Mode.Spectator)
+        {
+            spectorPlayer.SetActive(true);
+        }
+    }
+
+    public void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+        if(Input.GetKey(KeyCode.W))
+        {
+            spectorPlayer.transform.position += spectorPlayer.transform.forward * 100 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            spectorPlayer.transform.position -= spectorPlayer.transform.forward * 100 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            spectorPlayer.transform.position -= spectorPlayer.transform.right * 100 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            spectorPlayer.transform.position += spectorPlayer.transform.right * 100 * Time.deltaTime;
+        }
+        float rotateSpeed = 100;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            spectorPlayer.transform.rotation *= Quaternion.Euler(0, -rotateSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            spectorPlayer.transform.rotation *= Quaternion.Euler(0, rotateSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            spectorPlayer.transform.rotation *= Quaternion.Euler(-rotateSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            spectorPlayer.transform.rotation *= Quaternion.Euler(rotateSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
