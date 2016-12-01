1aecab2494ef30a35d8e018ad7536c7f95d72071
DZebraman <rpark682@gmail.com>
2016-12-01 18:22:20 -0500

    jeep stuff works with controller

---
+++ Assets/TankJeep/BarrelMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelMovement : MonoBehaviour {
+	
+    public GameObject firePos;
+    public GameObject bullet;
+    public float fireRate;
+
+    bool canFire = true;
+
+    IEnumerator fireCoolDown() {
+        yield return new WaitForSeconds(fireRate);
+        canFire = true;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        float uD = Input.GetAxis("Right Stick Y") * 10;
+
+        if(uD > 0.5f || uD < -0.5f) {
+            transform.Rotate(-uD,0,0);
+        }
+
+        if(Input.GetButton("Fire1") && canFire) {
+            Rigidbody temp = ((GameObject)Instantiate(bullet,firePos.transform.position,Quaternion.identity)).GetComponent<Rigidbody>();
+            temp.velocity = firePos.transform.forward * 100;
+            canFire = false;
+            StartCoroutine(fireCoolDown());
+        }
+	}
+}
---
+++ Assets/TankJeep/MountPivot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MountPivot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	    float lR = Input.GetAxis("Right Stick X") * 10;
+        if(lR > 0.5f || lR < -0.5f) {
+            transform.Rotate(0,-lR,0);
+        }
+	}
+}
