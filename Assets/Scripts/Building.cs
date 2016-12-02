bf3975af51f1fb13aedabd2cf1fc39823c7ed8c7
Colton Crawford (RIT Student) <cac5432@ad.rit.edu>
2016-12-01 20:50:51 -0500

    Working on building destruction.

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+
+    public int health;
+
+	// Use this for initialization
+	void Start () {
+        health = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/HandDestroyer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandDestroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<Building>().health -= 1;
+        int health = collision.gameObject.GetComponent<Building>().health;
+        if (health <= 0)
+        {
+            collision.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            collision.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            //Destroy(other.gameObject);
+        }
+    }
+}
