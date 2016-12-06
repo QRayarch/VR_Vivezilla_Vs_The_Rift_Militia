45d760113133bffa02937937e58928b0d4fe85fd
Alec Linder (RIT Student) <afl4947@ad.rit.edu>
2016-12-06 18:24:16 -0500

    Finished up the visuals for the players.

---
+++ Assets/Scripts/StretchArm.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+[ExecuteInEditMode]
+[DisallowMultipleComponent]
+public class StretchArm : MonoBehaviour {
+
+    public Transform shoulder;
+    public Transform hand;
+
+    private LineRenderer lineRen;
+
+	// Use this for initialization
+	void Start () {
+        lineRen = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lineRen.SetPosition(0, shoulder.position);
+        lineRen.SetPosition(1, hand.position);
+	}
+}
