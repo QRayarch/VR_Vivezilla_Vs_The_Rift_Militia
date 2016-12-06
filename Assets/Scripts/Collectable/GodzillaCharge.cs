e7f13b6ec87abf9a439d41f3593658b76bc5b142
John Park (RIT Student) <jrp3024@ad.rit.edu>
2016-12-06 17:56:22 -0500

    collectables

---
+++ Assets/Scripts/Collectable/GodzillaCharge.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GodzillaCharge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
